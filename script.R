2b06ccff0b8433efe1c7ffa01cc8418cc013517c
Brett Shwom <brett.shwom@nyu.edu>
2015-03-17 01:01:01 -0400

    q4

--- script.R
+++ script.R
@@ -67,3 +67,33 @@ aggregatedBaltimoreOnlyTable <- baltimoreOnlyTable[, list(totalEmissions = sum(E
 ggplot(aggregatedBaltimoreOnlyTable, aes(year, y = totalEmissions, color = type)) + geom_point() + geom_line()
 
 dev.off()
+
+################
+## Question 4 ##
+################
+
+# ~~uncomment me before submission~~
+# NEI <- readRDS("summarySCC_PM25.rds")
+# SCC <- readRDS("Source_Classification_Code.rds")
+
+png("plot4.png")
+
+# install.packages('data.table')
+
+library(ggplot2)
+
+require(data.table)
+
+coalRelatedCodes <- SCC[grep("[Cc]oal", SCC$Short.Name),]$SCC
+
+coalCombustionOnly <- NEI[NEI$SCC %in% coalRelatedCodes,]
+
+coalCombustionOnlyTable <- data.table(coalCombustionOnly)
+
+aggregatedCoalCombustionOnlyTable <- coalCombustionOnlyTable[, list(totalEmissions = sum(Emissions)), by = list(year,type )]
+
+ggplot(aggregatedCoalCombustionOnlyTable, aes(year, y = totalEmissions, color = type)) + geom_point() + geom_line()
+
+dev.off()
+
+
