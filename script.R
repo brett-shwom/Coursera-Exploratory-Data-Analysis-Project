1292049b1113c7ef3bc0daf614821119402562af
Brett Shwom <brett.shwom@nyu.edu>
2015-03-17 09:19:21 -0400

    5+6

--- script.R
+++ script.R
@@ -96,4 +96,57 @@ ggplot(aggregatedCoalCombustionOnlyTable, aes(year, y = totalEmissions, color = 
 
 dev.off()
 
+################
+## Question 5 ##
+################
+
+# ~~uncomment me before submission~~
+# NEI <- readRDS("summarySCC_PM25.rds")
+# SCC <- readRDS("Source_Classification_Code.rds")
+
+png("plot5.png")
+
+# install.packages('data.table')
+
+library(ggplot2)
+
+require(data.table)
+
+motorVehiclesOnly <- NEI[NEI$type == "ON-ROAD",]
+
+motorVehiclesOnlyTable <- data.table(motorVehiclesOnly)
+
+aggregatedMotorVehiclesOnly <- motorVehiclesOnlyTable[, list(totalEmissions = sum(Emissions)), by = list(year,type )]
+
+ggplot(aggregatedMotorVehiclesOnly, aes(year, y = totalEmissions, color = type)) + geom_point() + geom_line()
+
+dev.off()
+
+################
+## Question 6 ##
+################
+
+# ~~uncomment me before submission~~
+# NEI <- readRDS("summarySCC_PM25.rds")
+# SCC <- readRDS("Source_Classification_Code.rds")
+
+png("plot6.png")
+
+# install.packages('data.table')
+
+baltimoreAndLAVehiclesOnly = NEI[NEI$type == "ON-ROAD" & (NEI$fips == "24510" | NEI$fips == "06037") ,]
+
+baltimoreAndLAVehiclesOnly$city = ordered(baltimoreAndLAVehiclesOnly$fips, levels=c("24510", "06037"), labels=c("Baltimore", "Los Angeles"))
+
+library(ggplot2)
+
+require(data.table)
+
+baltimoreAndLAVehiclesOnlyTable <- data.table(baltimoreAndLAVehiclesOnly)
+
+aggregatedBaltimoreAndLAVehiclesOnly <- baltimoreAndLAVehiclesOnlyTable[, list(totalEmissions = sum(Emissions)), by = list(year,type,city )]
+
+ggplot(aggregatedBaltimoreAndLAVehiclesOnly, aes(year, y = totalEmissions, color = city)) + geom_point() + geom_line()
+
+dev.off()
 
